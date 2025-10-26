4c077b41f7368e751b1d4d00040482a01ed3513f
Oleksii Ahieiev <djamply@gmail.com>
2025-10-26 03:52:37 +0200

    Revert "Add dynamic background color flashing effect"
    This reverts commit 25bffd31830d318a2545fdbc7582cbc7f1ba99c7.

--- js/tap-the-deejay.js
+++ js/tap-the-deejay.js
@@ -5,12 +5,12 @@ import TapDJController  from './controller/tap-dj_controller.js';
 // Application Entry Point
 window.onload = function() {
 	// Get the container element for the view
-    const bodyElement = document.getElementById('app');
+    const appElement = document.getElementById('app');
 
     // Instantiate the Model, View, and Controller
     const tapModel = new TapDJModel();
 
-    const tapView = new TapDJView(bodyElement); 
+    const tapView = new TapDJView(appElement); 
     const tapController = new TapDJController(tapModel, tapView);
 
     // Initialize the application
